8dbad82989138603c4e22252b333f1366a8c344d
Jhon <jhon122.kmo@gmail.com>
2022-06-19 22:33:11 -0500

    Diseño Detalle

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const index = ({ news }) => {
-    return (
-        <div>
-            <img src={news.urlToImage} className="d-block w-100" alt="..." style={{ maxHeight: "450px" }} />
-            <div className="carousel-caption d-none d-md-block  bg-dark text-white">
-                <h5>{news.title}</h5>
-                <p>{news.description}</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <img
+        src={news.urlToImage}
+        className="d-block w-100"
+        alt="..."
+        style={{ maxHeight: "450px" }}
+      />
+      <div
+        className="carousel-caption d-none d-md-block  bg-dark text-white"
+        style={{ position: "inherit" }}
+      >
+        <h5>{news.title}</h5>
+        <p>{news.description}</p>
+      </div>
+    </div>
+  );
+};
 
-export default index
+export default index;
--- src/components/NewsContainer/index.js
+++ src/components/NewsContainer/index.js
@@ -1,41 +1,75 @@
-import React from 'react';
-import News from "../News"
+import React from "react";
+import News from "../News";
 
 const index = ({ noticia }) => {
-
-
-    return (
-        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-            </div>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <News news={noticia[0]} />
-                </div>
-                <div className="carousel-item">
-                    <News news={noticia[1]} />
-                </div>
-                <div className="carousel-item ">
-                    <News news={noticia[2]} />
-                </div>
-                <div className="carousel-item ">
-                    <News news={noticia[3]} />
-                </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+  return (
+    <div
+      id="carouselExampleCaptions"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="3"
+          aria-label="Slide 4"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <News news={noticia[0]} />
         </div>
-    )
-}
+        <div className="carousel-item">
+          <News news={noticia[1]} />
+        </div>
+        <div className="carousel-item ">
+          <News news={noticia[2]} />
+        </div>
+        <div className="carousel-item ">
+          <News news={noticia[3]} />
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
 
-export default index
+export default index;
