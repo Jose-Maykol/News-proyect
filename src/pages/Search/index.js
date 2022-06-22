9f445dc7928ee7a2a84713594c7b167c5848e6bc
Enrique-N <enrique.xe@gmail.com>
2022-06-21 22:50:40 -0500

    Search Validation

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -5,28 +5,32 @@ import { ApiContext } from '../../context';
 const Search = () => {
     const { LimitCurrent } = useContext(ApiContext)
 
+    console.log(LimitCurrent)
+
     return (
-        LimitCurrent
-            ? LimitCurrent.map(news =>
-                <div style={{ marginTop: "150px" }} key={news.id}>
-                    <div className="card d-flex flex-row mb-2 p-3 my-3 mx-5">
-                        <img
-                            src={news.urlToImage}
-                            className="card-img-top img-fluid w-50" alt="..."
-                        />
-                        <div className="card-body d-flex flex-column justify-content-between align-items-start ">
-                            <div className=" p-2 bg-primary rounded-3 my-1">{news.source.name}</div>
-                            <h5 className="card-title my-1">{news.title}</h5>
-                            <p className="card-text my-1">{news.description}</p>
-                            <div className="align-self-end">
-                                <Link to={`/detail/${news.id}`}>
-                                    <span className="btn btn-dark"> Leer más</span>
-                                </Link>
+        <div style={{ marginTop: "150px" }}>
+            {LimitCurrent.length > 0
+                ? LimitCurrent.map(news =>
+                    <div key={news.id}>
+                        <div className="card d-flex flex-row mb-2 p-3 my-3 mx-5">
+                            <img
+                                src={news.urlToImage}
+                                className="card-img-top img-fluid w-50" alt="..."
+                            />
+                            <div className="card-body d-flex flex-column justify-content-between align-items-start ">
+                                <div className=" p-2 bg-primary rounded-3 my-1">{news.source.name}</div>
+                                <h5 className="card-title my-1">{news.title}</h5>
+                                <p className="card-text my-1">{news.description}</p>
+                                <div className="align-self-end">
+                                    <Link to={`/detail/${news.id}`}>
+                                        <span className="btn btn-dark"> Leer más</span>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            ) : null
+                ) : <h1 style={{ marginTop: "10rem", textAlign: "center" }}>No hay Coincidencias</h1>}
+        </div>
     )
 }
 
