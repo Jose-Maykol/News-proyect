d2dbe7482c097e8214a816dc2f58d408a0fde9bf
José Maykol <jpaniura@unsa.edu.pe>
2022-06-23 15:59:22 -0500

    Featured news update

--- src/components/FeaturedNews/index.js
+++ src/components/FeaturedNews/index.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { ApiContext } from '../../context';
-import NewsContainer from '../NewsContainer';
+import "./FeaturedNews.css"
 
 const FeaturedNews = () => {
 
@@ -9,10 +9,20 @@ const FeaturedNews = () => {
   return (
     <div className="row" style={{ margin: "50px 30px 50px 30px" }}>
       {Feature ?
-        <div>
-          <div className="row col-12 d-flex justify-content-center my-2 ">
-            <NewsContainer noticia={Feature} />
-          </div>
+          <div className="featured-news-container" >
+          {Feature.map( featuredNews =>
+            <div className="featured-news-item">
+              <img src={featuredNews.urlToImage} alt={featuredNews.title}/>
+              <div className="featured-news-content">
+                <div className={`featured-news-name ${featuredNews.source.name.replace(/ /g, "").replace(/\(|\)/g, "")}`}>
+                  {featuredNews.source.name}
+                </div>
+                <h3>{featuredNews.title}</h3>
+                <p>{featuredNews.description}</p>
+                <a href="/"> Leer mas </a>
+              </div>
+            </div>
+          )}
         </div>
         : <div className="spinner-border" role="status">
           <span className="visually-hidden">Loading...</span>
