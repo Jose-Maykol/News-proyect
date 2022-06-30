4b7cf4d9f11225fa27459fe43697a7a6ca1976e0
José Maykol <jpaniura@unsa.edu.pe>
2022-06-30 18:15:33 -0500

    Page payment responsive

--- src/pages/Subscribe/index.js
+++ src/pages/Subscribe/index.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Subscribe.css'
 import SubscriptionOption from '../../components/SubscriptionOption'
 
 const Subcribe = () => {
+
+  const [changePlan, setChangePlan] = useState(false);
+
+  const handleChange = () => {
+    setChangePlan(!changePlan)
+  }
+
   const premiun = {
     name: 'premium',
     price: '50',
@@ -16,13 +23,27 @@ const Subcribe = () => {
   }
 
   return (
-    <div className='subcribe-content'>
+    <div className='subcribe-container'>
+      <div className='subcribe-content'>
       <h1>NUESTROS PLANES DE SUBCRIPCION</h1>
       <h2>Para que te mantengas mejor informado de todo lo que acontece en el mundo.</h2>
       <div className='suscribe-options'>
-        <SubscriptionOption typeSubscription={normal} />
-        <SubscriptionOption typeSubscription={premiun} />
+        <div className='suscribe-plans'>
+          {changePlan 
+            ? 
+            <SubscriptionOption typeSubscription={normal} />
+            :
+            <SubscriptionOption typeSubscription={premiun} />
+            }
+        </div>
+        <div className='suscribe-select'>
+          <input type="checkbox" id="switch" onChange={handleChange}/>
+          <label htmlFor="switch" >Toggle</label>
+          <p> Cambiar de plan</p>
+        </div>
       </div>
+    </div>
+      <img src="/img/subcripcion.jpg" alt="subcripcion" />
     </div>
   )
 }
